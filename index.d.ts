75a08fa1b350166efdf99e7db6f788a2d46dade2
Jonathan Skeate <skeate@users.noreply.github.com>
2019-02-20 19:37:05 -0500

    Add query to history's Location interface

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,10 @@ type Stringify = (query: any) => string;
 type Parse = (query: string) => any;
 declare const historyWithQuery: (history: History, stringify: Stringify, parse: Parse) => History;
 
+declare module 'history' {
+  interface Location {
+    query: { [key: string]: string };
+  }
+}
+
 export default historyWithQuery;
